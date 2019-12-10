34c528ba83728a58363d209db6692d4c4d255909
Lumi Bytyci <lumbytyci@hotmail.com>
2019-12-10 11:01:02 -0800

    Add primary token parsing and its test

--- rd_parser.py
+++ rd_parser.py
@@ -10,6 +10,22 @@ class ExpressionParser():
     def eof(self) -> bool:
         return self.cur >= len(self.expr)
 
+    def primary(self) -> float:
+        start_idx = self.cur
+        while not self.eof and self.expr[self.cur].isnumeric():
+            self.advance()
+            if not self.eof and self.expr[self.cur] == '.':
+                self.advance()
+
+        return float(self.expr[start_idx:self.cur])
+
 
 if __name__ == "__main__":
-    pass
+    while True:
+        try:
+            expr = input('>>> ')
+            parser = ExpressionParser(expr)
+            print(parser.primary())
+        except KeyboardInterrupt:
+            print()
+            exit()
--- tests/test_rd_parser.py
+++ tests/test_rd_parser.py
@@ -1,1 +1,13 @@
-import rd_parser
+from rd_parser import ExpressionParser
+
+
+def test_primary_integer_parsing():
+    expr = '123'
+    parser = ExpressionParser(expr)
+    assert parser.primary() == 123
+
+
+def test_primary_float_parsing():
+    expr = '127902.3180128392820226969'
+    parser = ExpressionParser(expr)
+    assert parser.primary() == 127902.3180128392820226969
